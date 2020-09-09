5c94cdc12f68253beb0d3ab8fade7eb5f213e0ec
Ashley Redman <ashredman@hotmail.com>
2020-09-09 15:28:06 +0100

    Added base style sheets

---
+++ public/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/css/normalize.css">
+    <link rel="stylesheet" href="dist/css/fonts.css">
+    <link rel="stylesheet" href="dist/css/vars.css">
+    <link rel="stylesheet" href="dist/css/index.css">
+    <title>Dale Studios Design - Behind the scenes</title>
+</head>
+
+<body>
+    <header></header>
+    <main>
+        <span class="backing-text">Lorem ipsum</span>
+        <h1>Lorem <strong>ipsum</strong></h1>
+        <h2>Lorem ipsum</h2>
+        <h3>Lorem ipsum</h3>
+        <h4>Lorem ipsum</h4>
+        <h5>Lorem ipsum</h5>
+        <h6>Lorem ipsum</h6>
+        <p>
+            Lorem ipsum <strong>dolor sit</strong> amet consectetur <a href="#">adipisicing elit</a>. Maiores, quod fugit! Consectetur natus veniam dolorem labore in voluptates expedita aut sit voluptatibus
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, quo similique temporibus consectetur quas vel facere quos recusandae reprehenderit aut omnis veniam, delectus beatae in odio numquam hic nobis vitae.
+            Corrupti voluptatibus corporis eos quaerat, quibusdam aliquid nulla provident consequatur iusto! Quos libero rem alias temporibus eum natus nemo dolorum doloremque possimus accusantium voluptatum perferendis nam, voluptatem, provident quis veniam.
+            Alias deleniti praesentium dolorem eum perferendis. Enim, dicta. Blanditiis dolores quaerat dicta, nemo dolorem suscipit ex vitae deleniti, sed, saepe esse nobis. Nesciunt fugiat accusamus expedita cum architecto, et dolor.
+        </p>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic ex doloremque, totam et libero quos harum! Aspernatur omnis unde voluptatum sed enim accusantium delectus? Sapiente ullam a odit nobis minima!
+            Dolorum soluta totam asperiores, ex aspernatur veritatis rerum placeat ullam dolore incidunt iure deleniti doloremque suscipit facilis libero error, dolorem necessitatibus autem iste! Saepe provident tempore magnam, autem itaque enim?
+            Non debitis incidunt neque facere itaque eum iusto eos sit voluptatem! Totam nisi ducimus quaerat dolores ratione, eveniet veniam quod, suscipit tempore vel tempora, id quidem quasi minus quibusdam vitae?
+            Quidem, qui est. Qui dignissimos magnam eius doloribus omnis culpa consequatur ipsum voluptatem tenetur minus asperiores aliquam sunt adipisci, id ullam quibusdam nobis iusto possimus non animi, voluptate aut saepe.
+            Delectus omnis veritatis nesciunt odio ducimus dolorum perspiciatis dolore ea, repudiandae fugiat quis repellendus recusandae pariatur reiciendis natus aliquam incidunt fugit optio corrupti a nam possimus magnam explicabo. Veritatis, explicabo.
+            Officiis ipsam ipsum, unde optio adipisci dolorum et rem, aspernatur porro ea qui praesentium itaque, explicabo nesciunt in laboriosam dolorem voluptate! Molestias enim architecto doloribus similique aut in ducimus blanditiis.
+        </p>
+
+        <ul>
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+        </ul>
+
+        <ol>
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum dolor sit <strong>amet consectetur</strong> adi? Cupiditate nisi quas enim!</li>
+            <li>Lorem <a href="">ipsum</a></li>
+        </ol>
+
+        <dl>
+            <dd>Lorem ipsum</dd>
+            <dd>Lorem ipsum</dd>
+            <dd>Lorem ipsum</dd>
+        </dl>
+        <hr>
+
+        <div class="swatch-grid">
+            <span class="swatch bg-white"></span>
+            <span class="swatch bg-black"></span>
+            <span class="swatch bg-off-white"></span>
+            <span class="swatch bg-light-grey"></span>
+            <span class="swatch bg-mid-grey"></span>
+            <span class="swatch bg-dark-grey"></span>
+            <span class="swatch bg-green"></span>
+            <span class="swatch bg-magenta"></span>
+            <span class="swatch bg-orange"></span>
+            <span class="swatch bg-yellow"></span>
+            <span class="swatch bg-mint"></span>
+            <span class="swatch bg-blue"></span>
+            <span class="swatch bg-red"></span>
+        </div>
+
+        <hr>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
